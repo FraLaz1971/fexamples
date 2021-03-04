0643b897d638bf02cafdb87a258a90a8264ad096
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-03-04 23:18:58 +0100

    after test on salford FTN77

--- src/solvequad.f
+++ src/solvequad.f
@@ -7,14 +7,16 @@ C and solves for the roots of the equation (FORTRAN 77 style).
 C
 C Get the coefficients of the quadratic equation.
 C
-        REAL A,B,C,DISCR
+        REAL A,B,C,DISCR,TEMP
+        LOGICAL TEMP1,TEMP2
         WRITE (*,*) 'Computing solutions for'
-        WRITE (*,*) 'Equation A * X**2 + B * X + C '
+        WRITE (*,*) 'Equation A * X**2 + B * X + C = 0'
 90      WRITE (*,*) 'Enter the coefficients A B and C: '
         WRITE (*,*) '(with A not eq. to 0) :'
 	    READ (*,*) A, B, C
 C
-	    IF ((A - 0).LT.0.000001) THEN 
+	WRITE (*,100) 'ABS(A) is: ',  ABS(A)
+	    IF (ABS(A).LT.0.000001) THEN 
             WRITE (*,*) 'Illegal value for coeff. A'
             GOTO 90
 	    END IF
@@ -22,25 +24,25 @@ C
 C Echo the coefficients to make sure they are entered correctly.
 C
 	WRITE (*,100) 'The coefficients are: ',  A, B, C
-100 	FORMAT (1X,A,3F10.4)
 C
 C Check the discriminant and calculate its roots.
 C
-	DISCR = B**2 - 4.*A*C
-	WRITE (*,*) 'Discriminant (= B**2 - 4.*A*C) is ', DISCR
+	DISCR = B**2.0 - 4.0*A*C
+	WRITE (*,*) 'Discriminant (= B**2 - 4*A*C) is ', DISCR
 	IF ( DISCR .LT. 0) THEN
 		WRITE (*,*) 'This equation has complex roots:'
-		WRITE (*,*) 'X_1 = ', -B/(2.*A),  '+i *',
+		WRITE (*,*) 'X_1 = ', -B/(2.0*A),  '+i *',
      &	SQRT(ABS(DISCR))/(2.*A)
-		WRITE (*,*)  'X_2 = ', -B/(2.*A),  '-i *',
+		WRITE (*,*)  'X_2 = ', -B/(2.0*A),  '-i *',
      &	SQRT(ABS(DISCR))/(2.*A)
-		ELSE IF ( ((B**2 - 4.*A*C) - 0).LT.0.000001) THEN
+      ELSE IF (ABS(DISCR).LT.0.000001) THEN
 		WRITE (*,*)  'Equation with single repeated real root:'
-		WRITE (*,*)  'X_1 = X_2 =', -B/(2.*A)
-	ELSE
-		WRITE (*,*)  'Equation with two distinct real roots:'
-		WRITE (*,*) 'X_1 =', (-B + SQRT(ABS(DISCR)))/(2.*A)
-		WRITE (*,*) 'X_2 =', (-B - SQRT(ABS(DISCR)))/(2.*A)
+		WRITE (*,*)  'X_1 = X_2 =', -B/(2.0*A)
+	ELSE IF ( DISCR .GT. 0.0) THEN
+        WRITE (*,*)  'Equation with two distinct real roots:'
+            WRITE (*,*) 'X_1 =', (-B + SQRT(ABS(DISCR)))/(2.0*A)
+            WRITE (*,*) 'X_2 =', (-B - SQRT(ABS(DISCR)))/(2.0*A)
 	END IF
 C
+100 	FORMAT (1X,A,3F10.6)
 	END
